6b2beb7f7a4f1012a11785d5f2781ddcb1a46862
sudharsfinale <sudharsfinale@gmail.com>
2025-03-19 09:34:42 +0530

    Added routes to view components and progress bar component in progress

---
+++ src/containers/FileExplorerContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FileExplorer from "../../components/FileExplorer";
+const FileExplorerContainer = () => {
+  return (
+    <div>
+      <h3>FileExplorerContainer</h3>
+      <FileExplorer />
+    </div>
+  );
+};
+
+export default FileExplorerContainer;
